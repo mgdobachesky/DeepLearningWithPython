359f3b5d96f5de722eac8c58a59bfac09f859bdf
Michael Dobachesky <mgdobachesky@gmail.com>
2020-11-22 16:01:20 -0500

    Simple multiclass classification

--- MulticlassClassification/Newswires.py
+++ MulticlassClassification/Newswires.py
@@ -1,14 +1,14 @@
-from keras.datasets import imdb
-from keras import models
-from keras import layers
+from keras.datasets import reuters
+from keras.utils import to_categorical
+from keras import models, layers
 import numpy as np
 import matplotlib.pyplot as plt
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
-word_index = imdb.get_word_index()
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+word_index = reuters.get_word_index()
 reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
-def decode_sequence(sequence):
+def decode(sequence):
     return ' '.join([reverse_word_index.get(i - 3, '?') for i in sequence])
 
 def graph_data(train_data, validate_data, train_label, validate_label, title, filename):
@@ -34,62 +34,65 @@ def vectorize_sequences(sequences, dimension=10000):
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
-y_train = np.asarray(train_labels).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
+one_hot_train_labels = to_categorical(train_labels)
+one_hot_test_labels = to_categorical(test_labels)
 
 # Build Network
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
 
-model.compile(optimizer='rmsprop', 
-              loss='binary_crossentropy',
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # Validate Approach
-partial_x_train = x_train[10000:]
-partial_y_train = y_train[10000:]
-x_validation = x_train[:10000]
-y_validation = y_train[:10000]
+x_validate = x_train[:1000]
+y_validate = one_hot_train_labels[:1000]
+partial_x_train = x_train[1000:]
+partial_y_train = one_hot_train_labels[1000:]
 
 history = model.fit(partial_x_train,
                     partial_y_train,
                     epochs=20,
                     batch_size=512,
-                    validation_data=(x_validation, y_validation))
+                    validation_data=(x_validate, y_validate))
 
-history_dict = history.history
-loss = history_dict['loss']
-val_loss = history_dict['val_loss']
-accuracy = history_dict['accuracy']
-val_accuracy = history_dict['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
 
 graph_data(train_data=loss, 
            validate_data=val_loss,
            train_label='Training loss',
            validate_label='Validation loss',
            title='Training and validation loss',
-           filename='MovieReviews_TrainingAndValidationLoss')
+           filename='Newswires_TrainingAndValidationLoss')
 
 graph_data(train_data=accuracy, 
            validate_data=val_accuracy,
            train_label='Training accuracy',
            validate_label='Validation accuracy',
            title='Training and validation accuracy',
-           filename='MovieReviews_TrainingAndValidationAccuracy')
+           filename='Newswires_TrainingAndValidationAccuracy')
 
 # Build Improved Network
 model = models.Sequential()
-model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-model.add(layers.Dense(16, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
 
-model.compile(optimizer='rmsprop', 
-              loss='binary_crossentropy',
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=4, batch_size=512)
-results = model.evaluate(x_test, y_test)
+model.fit(partial_x_train,
+          partial_y_train,
+          epochs=9,
+          batch_size=512,
+          validation_data=(x_validate, y_validate))
 
+results = model.evaluate(x_test, one_hot_test_labels)
 print(results)
